6a7e59ba9a276b91d9897fbe7c56a32f4ca6cb74
Baturhan Yılmaz <98781457+yilmazbaturhan@users.noreply.github.com>
2026-03-05 20:26:18 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:26:08.467Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:26:16.866Z
+Commit: Patch API endpoint error handling
