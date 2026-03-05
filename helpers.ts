ce70f16c0b80777edd2b4af4f04c863a80f701ab
Baturhan Yılmaz <98781457+yilmazbaturhan@users.noreply.github.com>
2026-03-05 20:26:29 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:23:47.649Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:26:27.730Z
+Commit: Add export functionality for reports
