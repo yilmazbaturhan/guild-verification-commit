8f166e154810796986a623c08c22c0c0762baf50
Baturhan Yılmaz <98781457+yilmazbaturhan@users.noreply.github.com>
2026-03-05 20:22:31 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:22:03.746Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T17:22:29.661Z
+Commit: Patch security vulnerability in authentication
