7ba72ab5d1ba913eab4a71541fc43220ae0ebf80
Baturhan Yılmaz <98781457+yilmazbaturhan@users.noreply.github.com>
2026-03-05 20:22:29 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:22:24.228Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T17:22:27.772Z
+Commit: Resolve null pointer exception in user service
