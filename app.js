08321b521c9817b32fb908d75a0bfecd36e44989
Baturhan Yılmaz <98781457+yilmazbaturhan@users.noreply.github.com>
2026-03-05 20:21:55 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:21:29.933Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T17:21:54.098Z
+Commit: Resolve null pointer exception in user service
