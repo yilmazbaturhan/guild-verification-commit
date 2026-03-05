9039197e3206292fa3198e87f1bd9d7802a91e0e
Baturhan Yılmaz <98781457+yilmazbaturhan@users.noreply.github.com>
2026-03-05 20:26:27 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:26:23.858Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T17:26:25.250Z
+Commit: Resolve timezone issues in date handling
