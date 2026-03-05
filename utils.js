d06c392b92ea2ad58da51917b9e6d8aa3b18c9e5
Baturhan Yılmaz <98781457+yilmazbaturhan@users.noreply.github.com>
2026-03-05 20:21:51 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:19:34.058Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T17:21:50.090Z
+Commit: Introduce real-time notifications
