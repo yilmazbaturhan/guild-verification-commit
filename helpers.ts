97c1179ab34357ae9b180e5ef02ede328119b461
Baturhan Yılmaz <98781457+yilmazbaturhan@users.noreply.github.com>
2026-03-05 20:21:39 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:19:42.042Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T17:21:38.156Z
+Commit: Correct typo in configuration file
