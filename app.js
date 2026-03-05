4758833d909355962ecf162a4b99d09600bab3cd
Baturhan Yılmaz <98781457+yilmazbaturhan@users.noreply.github.com>
2026-03-05 20:26:13 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:26:00.162Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T17:26:11.394Z
+Commit: Fix broken links in documentation
